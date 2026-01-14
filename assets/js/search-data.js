e2e31a1d89a1a29c97b5633be3cae63378ac0c3e
dksasaki <17645581+dksasaki@users.noreply.github.com>
2026-01-14 15:57:28 +0000

    Deploying to gh-pages from @ peces-example/peces-example.github.io@2dfa20bd6390e5b2624b9c9f1933ef6357b77653 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -454,9 +454,9 @@ ninja.data = [{
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-programming-in-earth-science",
+          title: 'Programming in Earth Science',
+          description: "Introduction Programming in Earth Science",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",
